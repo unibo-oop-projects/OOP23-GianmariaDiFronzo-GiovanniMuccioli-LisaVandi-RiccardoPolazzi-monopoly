2d08402b49e48117da0f0def8410d78cb04faa21
Danilo Pianini <danilo.pianini@unibo.it>
2024-02-27 19:07:27 +0100

    ci: add the OOP machinery

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.danilopianini.gradle-java-qa") version "1.28.0"
 
-    id("org.danilopianini.unibo-oop-gradle-plugin") version "1.0.6"
+    id("org.danilopianini.unibo-oop-gradle-plugin") version "1.0.7-dev03-64cbefe"
 }
 
 
